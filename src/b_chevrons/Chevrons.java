27b92ac36ea8eb92f4a5bc52a535b3678338edd9
rhunter-NTatC <72524582+rhunter-NTatC@users.noreply.github.com>
2021-08-19 08:54:43 -0500

    Erased "Key" Material
    Erased all the answers from starter code

--- src/b_chevrons/Chevrons.java
+++ src/b_chevrons/Chevrons.java
@@ -78,73 +78,30 @@ public class Chevrons {
 		 * This method only need 1 statement!!!!!!!
 		 */
 		
-		getInfo();
+		
 		
 	}
+	
+	
+	
 	
 	/*
 	 * gets the width and number of chevrons from the user
 	 */
 	public static void getInfo() {
 		
-		Scanner inkey = new Scanner(System.in);
 		
-		System.out.print("How wide do you want the chevrons? ");
-		int width = inkey.nextInt();
-		System.out.print("How many chevrons do you want? ");
-		int numChev = inkey.nextInt();
-		
-		System.out.println();
-		
-		printChevrons(width, numChev);
 		
 	}
+	
+	
+	
 	
 	/*
 	 * This actually prints the chevrons
 	 */
 	public static void printChevrons(int width, int numChev) {
 		
-		//loops the number of Chevrons needed
-		for (int i = 0; i < numChev; i++) {
-			
-			//print 1 chevron going out
-			for (int j = 1; j <= width; j++) {
-				
-				//print 1 chevron row
-				for (int k = 1; k <= j; k++) {
-					if (k < j) {
-						System.out.print(" ");
-					}
-					else {
-						System.out.println(">");
-					}
-				}
-				
-				
-				
-			}
-			
-			//print 1 chevron coming back
-			for (int j = width - 1; j > 1; j--) {
-				
-				//print 1 chevron row
-				for (int k = 1; k <= j; k++) {
-					if (k < j) {
-						System.out.print(" ");
-					}
-					else {
-						System.out.println(">");
-					}
-				}
-				
-				
-				
-			}
-			
-			
-		}
-		System.out.println(">");
 		
 		
 		
